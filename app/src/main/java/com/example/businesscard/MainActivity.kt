ec97243c4b7fd1d3367f3fda82104f369436befc
ThomasTesta99 <ttesta99@yahoo.com>
2025-02-24 17:43:30 -0500

    second branch commit

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Studio testing the Second branch",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
