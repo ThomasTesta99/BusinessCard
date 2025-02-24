e2650612db6ec986126d715a7483a03fa49158bc
ThomasTesta99 <ttesta99@yahoo.com>
2025-02-24 17:38:07 -0500

    second commit

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BusinessCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android Studio",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
